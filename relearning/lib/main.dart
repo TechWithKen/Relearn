7b953ca7dc9be01982fb52814ec57e6002900cc9
techwithken <ti664469@gmail.com>
2024-12-16 18:16:17 +0100

    added images and the circular avatar to my card app

--- relearning/lib/main.dart
+++ relearning/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyCardApp());
+  runApp(const MyCardApp());
 }
 
 class MyCardApp extends StatelessWidget {
@@ -11,26 +11,19 @@ class MyCardApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Row(children: [
-            Container(width: 100.0, color: Colors.red),
-            SizedBox(
-              width: 30.0,
-            ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Container(width: 100.0, height: 100.0, color: Colors.yellow),
-                Container(width: 100.0, height: 100.0, color: Colors.green),
-              ],
-            ),
-            SizedBox(
-              width: 30.0,
-            ),
-            Container(width: 100.0, color: Colors.blue),
-          ]),
-        ),
+        backgroundColor: Colors.brown[900],
+        body: const SafeArea(
+            child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CircleAvatar(
+                radius: 100.0,
+                backgroundImage: AssetImage('images/Kehinde.jpg'),
+              ),
+            ],
+          ),
+        )),
       ),
     );
   }
